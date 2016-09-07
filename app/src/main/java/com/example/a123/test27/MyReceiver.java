59a42d34258ab82d0c659c6e502e31926e542411
wangsheng666 <1028742881@qq.com>
2016-09-07 11:36:50 +0800

    理解广播概念，广播的发送与接收相关知识

---
+++ app/src/main/java/com/example/a123/test27/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.a123.test27;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String strMsg=intent.getStringExtra("broadcastmsg");
+        Toast.makeText(context,strMsg,Toast.LENGTH_LONG).show();
+    }
+}
